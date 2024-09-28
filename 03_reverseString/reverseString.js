559279cc60a5ebefc94563cdcbe212fb787e4ad0
daliduraku <mesherbetmolla@gmail.com>
2024-09-27 22:34:43 -0400

    Completed the set of exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str === ""){
+        return ""
+    }
+    const arr = str.split("");
+    const tempArr = [];
+    for (let i = arr.length - 1; i >= 0; i--){
+        tempArr.push(arr[i])
+    }
+    const finalString = tempArr.join("");
+    return finalString
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(a, num1=0, num2=0, num3=0, num4=0) {
+    arr = [];
+
+    for ( let i = 0; i < a.length; i++){
+        if (a[i] !== num1 && a[i] !== num2 && a[i] !== num3 && a[i] !== num4){
+            arr.push(a[i])
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
